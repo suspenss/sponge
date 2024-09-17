a17a684eb7295363f4524a2a77aea19f96f338f2
eopche <tniasu@qq.com>
2024-09-17 07:46:10 +0000

    format code

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -7,8 +7,8 @@
 
 // You will need to add private members to the class declaration in `stream_reassembler.hh`
 
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+template<typename... Targs>
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -18,9 +18,13 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+  DUMMY_CODE(data, index, eof);
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const {
+  return {};
+}
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const {
+  return {};
+}
